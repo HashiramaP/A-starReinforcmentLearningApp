9c3a7ebb5da05205027bcdec0fde38e73f344bc4
Parsa Homayouni <parsa.homayouni@gmail.com>
2024-12-04 00:37:14 -0500

    disabled darkmode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "media", // Disable dark mode by using 'media' or 'class'
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
